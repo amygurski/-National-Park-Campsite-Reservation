c35357506767d18b65feb714c569e7951a1632b1
joancrookston-c <joanemily@live.com>
2020-02-19 15:28:27 -0500

    Added ViewParks method

--- 09_Capstone/Capstone/Models/Campground.cs
+++ 09_Capstone/Capstone/Models/Campground.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace Capstone.Models
 {
-    class Campground
+    public class Campground
     {
+        public int Id { get; set; }
+        public int ParkId { get; set; }
+        public string Name { get; set; }
+        public int OpenMonths { get; set; }
+        public int ClosedMonths { get; set; }
+        public decimal DailyFee { get; set; }
+        public Campground (int id, int parkId, string name, int openMonths, int closedMonths, decimal dailyFee)
+        {
+            Id = id;
+            ParkId = parkId;
+            Name = name;
+            OpenMonths = openMonths;
+            ClosedMonths = closedMonths;
+            DailyFee = dailyFee;
+        }
     }
 }
--- 09_Capstone/Capstone/Models/Park.cs
+++ 09_Capstone/Capstone/Models/Park.cs
@@ -9,19 +9,36 @@ namespace Capstone.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
-        public int EstablishDate { get; set; }
-        public double Area { get; set; }
+        public DateTime EstablishDate { get; set; }
+        public int Area { get; set; }
         public int AnnualVisitors { get; set; }
         public string Description { get; set; }
-        public Park (int id, string name, string location, int establishDate, double area, int annualVisitors, string description)
+        //public Park (int id, string name, string location, int establishDate, double area, int annualVisitors, string description)
+        //{
+        //    Id = id;
+        //    Name = name;
+        //    Location = location;
+        //    EstablishDate = establishDate;
+        //    Area = area;
+        //    AnnualVisitors = annualVisitors;
+        //    Description = description;
+        //}
+
+        public void ViewParks()
         {
-            Id = id;
-            Name = name;
-            Location = location;
-            EstablishDate = establishDate;
-            Area = area;
-            AnnualVisitors = annualVisitors;
-            Description = description;
+            List<Park> parks = ParksSqlDAO.GetParks();
+
+                Console.WriteLine($"Select a Park for Further Details: ");
+
+            foreach (Park park in parks)
+            {
+                Console.WriteLine($" {park.Id}. {park.Name}");
+            }
+            Console.WriteLine($"Please enter selection here: ");
+                string response = Console.ReadLine();
+                int selection = int.Parse(response);
+
+
         }
     }
 }
